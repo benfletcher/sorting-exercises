b3f048862035650109ccd7e9fdddfffacf3a6ca4
numbergames <bfletch@gmail.com>
2017-01-12 14:30:11 -0500

    basic algos written 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sorting-exercises
+
+## Bubble Sort
--- app.js
+++ app.js
@@ -56,9 +56,35 @@ function merge(left, right, ary) {
   return ary;
 }
 
+function quickSort(array, start, end) {
+  start = start === undefined ? 0 : start;
+  end = end === undefined ? array.length : end;
 
+  if (start >= end) {
+    return array;
+  }
 
+  var middle = partition(array, start, end);
+  array = quickSort(array, start, middle);
+  array = quickSort(array, middle + 1, end);
 
+  return array;
+}
+
+function partition(array, start, end) {
+  var pivot = array[end - 1];
+  var j = start;
+
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      [array[i], array[j]] = [array[i], array[j]];
+      j++;
+    }
+  }
+
+  [array[end - 1], array[j]] = [array[j], array[end - 1]];
+qui  return j;
+}
 
 
 
